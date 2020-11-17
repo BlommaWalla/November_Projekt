700a4bf1301330c82e2d764cd246b67fbeb03ed0
BlommaWalla <lutz.hahlbeckmail.com>
2020-11-17 12:38:34 +0100

    walla

---
+++ MLG_RPG_DVD_For_People_With_ADD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MLG_RPG_DVD_For_People_With_ADD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wallah");
+        }
+    }
+}
